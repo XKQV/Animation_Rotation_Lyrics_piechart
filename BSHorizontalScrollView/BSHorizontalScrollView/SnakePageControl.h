4d40bf84575691194551199799f31a765eabc5fa
Zhiwei <zhiwei_dong@foxmail.com>
2019-04-28 18:03:59 +0800

    Added oc.h .m for page come

---
+++ BSHorizontalScrollView/BSHorizontalScrollView/SnakePageControl.h
@@ -0,0 +1,17 @@
+//
+//  SnakePageControl.h
+//  BSHorizontalScrollView
+//
+//  Created by 董志玮 on 2019/4/28.
+//  Copyright © 2019 董志玮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SnakePageControl : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BSHorizontalScrollView/BSHorizontalScrollView/SnakePageControl.m
@@ -0,0 +1,21 @@
+//
+//  SnakePageControl.m
+//  BSHorizontalScrollView
+//
+//  Created by 董志玮 on 2019/4/28.
+//  Copyright © 2019 董志玮. All rights reserved.
+//
+
+#import "SnakePageControl.h"
+
+@implementation SnakePageControl
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
